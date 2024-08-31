977b3d72c2174da3ee879b3edaa232f000d11871
froehlichtobias <froehlich_tobias@icloud.com>
2024-08-31 20:06:32 +0200

    changed the h2 on the front page

--- src/App.js
+++ src/App.js
@@ -8,12 +8,9 @@ function App() {
     <div className="App">
       <h1>BreathHold3000</h1>
       <h2>
-        Hello Everyone! I created this webapp because I wanted to improve my
-        breathhold and I found out about an exercise you can do to improve it,
-        but I found it annoying to do on my phone with the stopwatch.<br></br>If
-        you want to save your data and look at your stats and improvement go
-        ahead and create an account, if not, you can just continue as a guest.
-        :)
+        Hello Everyone!<br></br>If you want to save your data and look at your
+        stats and improvement go ahead and create an account, if not, you can
+        just continue as a guest.
       </h2>
       <SignUp />
       <Login />
