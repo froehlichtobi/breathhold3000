1e6c3dcf06f284de03e0f88be46cf9ad4ced331b
Tobias Fröhlich <90284111+froehlichtobi@users.noreply.github.com>
2024-09-03 12:50:43 +0000

    created file for maxbreathhold test but just started declaring basic things

---
+++ src/maxBreathHold.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const maxBreathHold = () => {
+
+    const [seconds, setSeconds] = useState(0);
+    const [active, setActive] = useState(false);
+
+
+
+}
+
+
+export default maxBreathHold;
