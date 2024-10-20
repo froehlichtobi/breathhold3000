abb6a7de60652ba88b44008f63dbb395d08699f7
froehlichtobias <froehlich_tobias@icloud.com>
2024-10-20 13:05:10 +0200

    added href again to the header to changet he cursor icon when hovering

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <h1 className="header">
-      <a onClick={() => window.location.reload()}>BreathHold3000</a>
+      <a href="#" onClick={() => window.location.reload()}>
+        BreathHold3000
+      </a>
     </h1>
   );
 };
